e3ace9cf17391d8b0b15798b8d2937cd4dd743b2
chunchill <chunchill@163.com>
2013-12-12 00:13:17 +0800

    map pages mockup

--- source/ITMS.Web/scripts/app/tempAppointmentApp.js
+++ source/ITMS.Web/scripts/app/tempAppointmentApp.js
@@ -3,3 +3,9 @@ var tempAppointmentViewModel = new IMS.TempAppointmentViewModel();
 $(document).ready(function () {
     ko.applyBindings(tempAppointmentViewModel);
 });
+
+$(document).delegate("#theThirdStepView", "pageinit", function () {
+    $("#deliveryNotesOverview").listview("refresh");
+});
+
+
